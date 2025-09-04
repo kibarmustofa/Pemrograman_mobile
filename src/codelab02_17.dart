fbd77442ec28196cab3f1bad165a9b08ac98dd2c
kibarmustofa <kibarmustofa115@gmail.com>
2025-09-04 08:09:25 +0700

    tugas soal no 1

--- src/codelab02_17.dart
+++ src/codelab02_17.dart
@@ -1,5 +1,6 @@
 void main(){
-  for (int i = 0; i <10;i++ ){
-    print('hello ${i+2}');
+  String nama = "Kibar Mustofa";
+  for (int i = 18; i > 8; i-- ){
+    print('Nama saya adalah $nama, sekarang berumur $i');
   }
 }
